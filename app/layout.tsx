ca3b00b975b082c5bd26e07226d3c3566be7e64e
Jose M Delgado <jdelgado2002@gmail.com>
2025-03-26 09:54:01 -0500

    update app metadata with new title and description for AWS WorkSpaces Pricing Calculator

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Aufsite',
+  description: 'Aufsite AWS WorkSpaces Pricing Calculator',
   generator: 'v0.dev',
 }
 
